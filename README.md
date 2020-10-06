f334d50dd7b7be2a91c3709f3ab361543c330953
Fischer Frederico <frederico.fischer@visol.ch>
2020-10-06 17:17:30 +0200

    [FEATURE] Create Sidebar

--- README.md
+++ README.md
@@ -4,16 +4,21 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 In the CI/CD Lifecycle this application will be available on  http://wiproh20-owerlen.enterpriselab.ch/
 
 ## Available Scripts
-Start the app (available on Port 3000):
+Install all packages:
+
+### `npm install`
+
+
+Start the app (uses Port 3000):
 
 ### `npm start`
 
-Build Docker Image:
+If you want to use Docker, build Docker Image:
 
 ### `docker build --tag electrolernapp:1.0 .`
 
 
-Start the Docker Container:
+Start the Docker Container (uses Port 1337):
 
 ### `docker run -p 1337:80 -d --name electrolernapp electrolernapp:1.0`
 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, {Fragment} from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Sidebar from "react-sidebar";
 import NavigationBar from './Components/NavigationBar';
 import Home from "./Components/Home";
 import About from "./Components/About";
 import NoMatch from "./Components/NoMatch";
-import Sidebar from "./Components/Sidebar";
 
-function App() {
-  return (
-      <Fragment>
-        <Router>
-            <NavigationBar/>
-            <Sidebar/>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route component={NoMatch} />
-            </Switch>
-        </Router>
-      </Fragment>
-  );
+class App extends React.Component {
+
+
+    render() {
+        return (
+            <Fragment>
+                <Router>
+                    <Sidebar
+                        sidebar={<b>Sidebar content</b>}
+                        open={this.state.sidebarOpen}
+                        docked={this.state.sidebarDocked}
+                        onSetOpen={this.onSetSidebarOpen}
+                    >
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/about" component={About}/>
+                            <Route component={NoMatch}/>
+                        </Switch>
+                    </Sidebar>
+                </Router>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
